d98ac7f9c35b22cf707528ae28e2036486a993b1
Sam <sam@ischool.berkeley.edu>
2013-02-14 14:40:07 -0800

    hw-stats first draft

--- code/stats.py
+++ code/stats.py
@@ -4,40 +4,90 @@ available from http://www.fec.gov/disclosurep/PDownload.do"""
 
 import fileinput
 import csv
+import math
 
 total = 0
+values = []
+candidates = {}
 
 for row in csv.reader(fileinput.input()):
     if not fileinput.isfirstline():
-        total += float(row[9])
+	value = float(row[9])
+	total += value
+	candidateId = str(row[1])
+	candidate = str(row[2])	
+	
+	if candidateId not in candidates:
+		candidates[candidateId] = candidate
+	
         ###
         # TODO: calculate other statistics here
         # You may need to store numbers in an array to access them together
         ##/
+	values.append(value)
 
 ###
 # TODO: aggregate any stored numbers here
 #
 ##/
+mean = total/len(values)
+median = sorted(values)[len(values)/2]
+minVal = min(values)
+maxVal = max(values)
 
+def sd(values,mean):
+	size = len(values)
+
+	#Find deviation from mean for each value and square it
+	squaresOfDev = [(v-mean)**2 for v in values]
+	
+	#Sum the squared deviation of each value
+	sumOfDev = 0.0
+	for v in squaresOfDev:
+		sumOfDev += v
+
+	#Divide the sum of squared deviations by one less than the total count of values. Then find the square root.
+	stdDev = (sumOfDev/size)**0.5
+	
+	return stdDev
+
+def printCandidates(candidates):
+	
+	cList =[]
+		
+	#Split into first and last names. Reverse the order of first and last names. Remove commas. Add to list.	
+	for key in candidates:
+		
+		c = candidates[key].split()
+		cString = c[1]+" "+c[0]
+		cString = cString.replace(',','')
+		cList.append(cString)
+	
+	#Sort the list and then create a comma separated string from the list
+	cList = sorted(cList)
+	candidatesList = ", ".join(cList)
+
+	return candidatesList
+	
 ##### Print out the stats
 print "Total: %s" % total
-print "Minimum: "
-print "Maximum: "
-print "Mean: "
-print "Median: "
+print "Minimum: %s " %  minVal
+print "Maximum: %s" % maxVal
+print "Mean: %s" % str(mean)
+print "Median: %s" % str(median) 
 # square root can be calculated with N**0.5
-print "Standard Deviation: "
+print "Standard Deviation: %s" % str(sd(values,mean))
 
 ##### Comma separated list of unique candidate names
-print "Candidates: "
+print "Candidates: %s" % printCandidates(candidates)
 
 def minmax_normalize(value):
     """Takes a donation amount and returns a normalized value between 0-1. The
     normilzation should use the min and max amounts from the full dataset"""
     ###
     # TODO: replace line below with the actual calculations
-    norm = value
+    norm = ((value-minVal)/(maxVal-minVal))*(1-0)+0
+
     ###/
     
     return norm
