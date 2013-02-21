61da01e4f9784f44b4917b2c705dedc7261ec940
Sam <sam@ischool.berkeley.edu>
2013-02-21 00:33:53 -0800

    hw modified unique_review.py

--- code/unique_review.py
+++ code/unique_review.py
@@ -18,24 +18,37 @@ class UniqueReview(MRJob):
             #   yield [ ___ , ___ ]
             ##/
 
+	    #Added by Sam
+	    for word in WORD_RE.findall(record['text']):
+                yield [word.lower(), record['review_id']]
+
     def count_reviews(self, word, review_ids):
         """Count the number of reviews a word has appeared in.  If it is a
         unique word (ie it has only been used in 1 review), output that review
         and 1 (the number of words that were unique)."""
 
         unique_reviews = set(review_ids)  # set() uniques an iterator
+	
         ###
         # TODO: yield the correct pair when the desired condition is met:
         # if ___:
         #     yield [ ___ , ___ ]
         ##/
+	if len(unique_reviews) == 1:
+		yield [unique_reviews.pop(),1]
+		
+    #def do_nothing(self, review_id, 1):
+	#yield [review_id,1]
+
 
     def count_unique_words(self, review_id, unique_word_counts):
         """Output the number of unique words for a given review_id"""
-        ###
+        
+	###
         # TODO: summarize unique_word_counts and output the result
         # 
         ##/
+	yield review_id, sum(unique_word_counts)
 
     def aggregate_max(self, review_id, unique_word_count):
         """Group reviews/counts together by the MAX statistic."""
@@ -44,24 +57,32 @@ class UniqueReview(MRJob):
         # the same reducer:
         # yield ["MAX", [ ___ , ___]]
         ##/
+	yield ["MAX", [ unique_word_count , review_id ]]
 
     def select_max(self, stat, count_review_ids):
         """Given a list of pairs: [count, review_id], select on the pair with
         the maximum count, and output the result."""
-        ###
+	###
         # TODO: find the review with the highest count, yield the review_id and
         # the count. HINT: the max() function will compare pairs by the first
         # number
         #
-        #/
+        ##/
+	maxVal=max(count_review_ids)
+	
+	yield str(maxVal[0]), str(maxVal[1])
 
     def steps(self):
-        """TODO: Document what you expect each mapper and reducer to produce:
-        mapper1: <line, record> => <key, value>
-        reducer1: <key, [values]>
-        mapper2: ...
         """
-        return [self.mr(self.extract_words, self.count_reviews),
+        TODO: Document what you expect each mapper and reducer to produce:
+        mapper1: <line, record> => <word, review_id>
+        reducer1: <word, [review_ids]> => <review_id,1>
+        mapper2: <review_id, 1> => <review_id,1>
+        reducer2: <review_id, [1,1,...]> => <review_id,sum>
+        mapper3: <review_id, sum> => <MAX, [sum, review_id]>
+        reducer3: <MAX, [[sum, review_id],...]>, <review_id, sum> of the max(sum)
+	"""
+	return [self.mr(self.extract_words, self.count_reviews),
                 self.mr(reducer=self.count_unique_words),
                 self.mr(self.aggregate_max, self.select_max)]
 
