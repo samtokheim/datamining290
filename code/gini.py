eb1115652825d0cdda6211cfd021a31b2a076d15
Sam <sam@ischool.berkeley.edu>
2013-02-28 18:43:21 -0800

    2nd draft of gini.py

--- code/gini.py
+++ code/gini.py
@@ -6,7 +6,7 @@ Classes are strings."""
 import fileinput
 import csv
 import json
-from collections import defaultdict
+from collections import defaultdict,Counter
 
 (cmte_id, cand_id, cand_nm, contbr_nm, contbr_city, contbr_st, contbr_zip,
 contbr_employer, contbr_occupation, contb_receipt_amt, contb_receipt_dt,
@@ -17,9 +17,8 @@ receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = ran
 # TODO: declare datastructures
 
 cNames = defaultdict(int)
-zCodes = defaultdict(int)
+candy = defaultdict(lambda: defaultdict(int))
 counter = 0.0
-
 
 ############### Read through files
 for row in csv.reader(fileinput.input()):
@@ -34,13 +33,30 @@ for row in csv.reader(fileinput.input()):
 	zipCode=str(row[6])
 	counter+=1
 	cNames[candidateId]+=1
-	zCodes[zipCode]+=1	
+	candy[zipCode][candidateId]+=1
+
+split_gini_list = []
+for z in candy:
+	#Get the total number of donations for a zip code
+	zip_donations = float(sum(candy[z][c] for c in candy[z]))
+	
+	#Instantiate new list to store individual candidate gini scores per zipcode
+	#Calculate the gini score for each candidate/zipcode
+	candidates_zip_gini=[]
+	for c in candy[z]:
+		c_gini = (candy[z][c]/zip_donations)**2
+		candidates_zip_gini.append(c_gini)
+	#Take the gini score for a zip code and weight by dividing its donations by all the donations
+	weighted_score = (1-sum(candidates_zip_gini))*(zip_donations/counter)
+	
+	#Add this to a list so these weighted zip Gini scores can be summed into an average
+	split_gini_list.append(weighted_score)
 
 ###
 # TODO: calculate the values below:
 gini = 1-sum((cNames[c]/counter)**2 for c in cNames)  # current Gini Index using candidate name as the class
-zip_gini= 1-sum((zCodes[z]/counter)**2 for z in zCodes)
-split_gini = sum((cNames[c]/counter)*(1-(cNames[c]/counter)**2)*zip_gini for c in cNames) # weighted average of the Gini Indexes using candidate names, split up by zip code
+split_gini = sum(split_gini_list) # weighted average of the Gini Indexes using candidate names, split up by zip code
+
 ##/
 print "Gini Index: %s" % gini
 print "Gini Index after split: %s" % split_gini
