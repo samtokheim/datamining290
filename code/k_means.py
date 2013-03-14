ede2f81e8bea5415b9ed0fafcf15d553720ca315
Sam <sam@ischool.berkeley.edu>
2013-03-13 17:16:10 -0700

    first draft of hw-kmeans completed

--- code/k_means.py
+++ code/k_means.py
@@ -3,6 +3,9 @@
 #
 ##/
 
+import random
+import math
+
 dataset = [-13.65089255716321, -0.5409562932238607, -88.4726466247223, 39.30158828358612, 4.066458182574449, 64.64143300482378, 38.68269424751338, 33.42013676314311, 31.18603331719732, -0.2027616409406292, 45.13590038987272, 30.791899783552395, 61.1727490302448, 18.167220741624856, 88.88077709786394, -1.3808002119514704, 50.14991362212521, 55.92029956281276, -6.759813255299466, 34.28290084421072]
 k = 2 # number of clusters
 
@@ -15,21 +18,35 @@ def pick_centroids(xs, num):
     """Return list of num centroids given a list of numbers in xs"""
     ###
     # TODO select and return centroids
-    return [1,2]
+    
+    #Randomly chose k centroids from the dataset using sample without replacement
+    centroids = random.sample(xs,num)
+
+    return centroids
+
     ##/
 
 def distance(a, b):
     """Return the distance of numbers a and b"""
     ###
     # TODO return correct expression
-    return 0
+
+    #1-dimensional Euclidean distance between points a,b
+    distance = (a-b)**2
+    
+    return distance
+    #return 0
     ##/
 
 def centroid(xs):
     """Return the centroid number given a list of numbers, xs"""
     ###
     # TODO calculate and return centroid
-    return 0
+    
+    #calculate the centroid
+    centroid = sum(xs)/len(xs)
+    
+    return centroid
     ##/
 
 def cluster(xs, centroids):
