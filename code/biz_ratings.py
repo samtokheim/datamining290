4e7e24cb34487f777ec426704d38fb9af792b473
Sam <sam@ischool.berkeley.edu>
2013-04-19 12:08:27 -0700

    now calculates average stars for each category

--- code/biz_ratings.py
+++ code/biz_ratings.py
@@ -8,7 +8,6 @@ class AverageStars(MRJob):
     def extract_category(self, _, record):
         """Take in a record, filter by type=review, yield <category, stars>"""
         if record['type'] == 'business':
-		"""Yield each category in the category list"""
 		for c in record['categories']:
                 	yield c, record['stars']
 
@@ -21,6 +20,6 @@ class AverageStars(MRJob):
     #MapReduce Steps
     def steps(self):
 	return [self.mr(self.extract_category, self.average_stars)]
-	
+
 if __name__ == '__main__':
     AverageStars.run()
