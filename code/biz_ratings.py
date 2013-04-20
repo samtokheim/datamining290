8bd4d6b868375ffdcf39adbeca59429951780a6c
Sam <sam@ischool.berkeley.edu>
2013-04-20 14:33:44 -0700

    updated code to provide average REVIEW stars for each category

--- code/biz_ratings.py
+++ code/biz_ratings.py
@@ -9,14 +9,14 @@ class AverageStars(MRJob):
         """Take in a record, filter by type=review, yield <category, stars>"""
         if record['type'] == 'business':
 		for c in record['categories']:
-                	yield c, record['stars']
+                	yield c, [record['stars'],record['review_count']]
 
     #Reduce
     def average_stars(self,category, stars):
-	"""Take in the category and stars, yield <category, average stars>"""
-	star_list = list(stars)
-        yield str(category), str(sum(star_list)/len(star_list))
-    
+	"""Take in the category and stars, yield <category, average review stars>"""
+	star_list = list(stars)	
+	yield category, str(sum([s[0]*s[1] for s in star_list])/sum([s[1] for s in star_list]))
+
     #MapReduce Steps
     def steps(self):
 	return [self.mr(self.extract_category, self.average_stars)]
